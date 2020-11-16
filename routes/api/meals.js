e92878a0ee24cfd4565e484871d13cbbe027c3d4
Danny <tekhexrax@yahoo.com>
2020-11-16 18:14:02 -0500

    Moved index.js for routes to correct folder, added meal route, condensed the 4 meal models/controllers to 1 model and 1 controller, fixed port on server.js

---
+++ routes/api/meals.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const mealsController = require("../../controllers/mealController");
+
+// Matches with "/api/meals"
+router.route("/")
+  .get(mealsController.findAll)
+  .post(mealsController.create);
+
+// Matches with "/api/meals/:id"
+router
+  .route("/:id")
+  .get(mealsController.findById)
+  .delete(mealsController.remove);
+
+module.exports = router;
