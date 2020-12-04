a3a36cfe97ed986b0635f281cd46a52f72278419
Matthew Kerr <matt777k@gmail.com>
2020-12-04 15:10:33 -0500

    added the calorie calculator popover. Not currently functioning. Needs to be connected to javascript.

--- client/src/components/CalorieCalculatorCard.jsx
+++ client/src/components/CalorieCalculatorCard.jsx
@@ -8,7 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CalcImg from '../images/foodcalc.jpg'
-import {NavLink} from 'react-router-dom';
+import CalCalcPopover from './CalCalcPopover';
 
 const useStyles = makeStyles({
   root: {
@@ -23,12 +23,7 @@ export default function MediaCard() {
   const classes = useStyles();
 
   return (
-    <NavLink to="/calorieCalculator" style={{ textDecoration: 'none' }} className=
-    {window.location.pathname === "/calorieCalculator"
-      ? "nav-link active"
-      : "nav-link"}>
     <Card className={classes.root}>
-      <CardActionArea>
         <CardMedia
           className={classes.media}
           image={CalcImg}
@@ -41,9 +36,8 @@ export default function MediaCard() {
           <Typography variant="body2" color="textSecondary" component="p">
             This is the Calorie Calculator tool that is used to estimate your calorie needs based on your goal
           </Typography>
+         <CalCalcPopover></CalCalcPopover>
         </CardContent>
-      </CardActionArea>
     </Card>
-    </NavLink>
   );
 }
